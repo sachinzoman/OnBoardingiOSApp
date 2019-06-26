529bfe66a00c42d3ba766f352206c912a7ad711f
Sachin <sachin.b@zomato.com>
2019-06-26 11:10:42 +0530

    Initial Commit

---
+++ OnBoardingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnBoardingApp
+//
+//  Created by Sachin Singh on 26/06/19.
+//  Copyright © 2019 Sachin Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ OnBoardingAppTests/OnBoardingAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  OnBoardingAppTests.swift
+//  OnBoardingAppTests
+//
+//  Created by Sachin Singh on 26/06/19.
+//  Copyright © 2019 Sachin Singh. All rights reserved.
+//
+
+import XCTest
+@testable import OnBoardingApp
+
+class OnBoardingAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
